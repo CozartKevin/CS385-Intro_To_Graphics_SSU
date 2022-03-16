95876c42d80ca2143e997a8848c313416c892c96
Kevin Cozart <Cozartkevin@gmail.com>
2022-03-16 15:39:52 -0700

    Functional Visuals
    I have a sun, an earth and a moon.  Now to get them moving.  Thanks Dave!

--- Assignment 4/js/main.js
+++ Assignment 4/js/main.js
@@ -19,99 +19,62 @@ function init() {
    // Earths Orbit : 1.5
    // moon orbit :  0.0175
    // Overall Diameter: 2.6102 * 2 = D
-   var sun = new Sphere();
-   var earth = new Sphere();
-   var moon = new Sphere();
+   var sun = new Sphere(60,36);
+   var earth = new Sphere(60,36);
+   var moon = new Sphere(60,36);
 
 
     //Sun Settings
-    sun.radius = 109;
+    sun.radius = 2.09;
     sun.color = [0.970, 0.621, 0.165, 1];
     //Earth Settings
-    earth.radius = 1;
-    earth.distance = 1000;
+    earth.radius = 0.25;
+    earth.distance = 5;
     earth.color = [0.247, 0.657, 0.950, 1];
     //Moon Settings
-    moon.radius = 0.27;
-    moon.distance = 15;
+    moon.radius = 0.05;
+    moon.distance = 0.5;
     moon.color = [0.860, 0.845, 0.826, 1];
 
-  
+    var t = 0.0;
     
-   
+        var axis = [1,0,0]; 
+
 
     // Add your sphere creation and configuration code here
     render = function(){
-        var year = 100; 
-        var day = 0;
-        var axis = [1,1,0]; 
-    
+             
         var radius = 200;
 
 
         var D = 2 * (earth.distance + moon.distance + moon.radius); 
-        var fovy, aspect, zNear, zFar; 
+        var zNear = 100;
+        var zFar = zNear + D;
+        var fovy, aspect;
         var sin = (D/2) / (zNear + (D/2));
-        fovy = (2 * (Math.asin()));
+        fovy = (2 * ((Math.asin(sin) * (180/Math.PI))));
         aspect = canvas.clientWidth/canvas.clientHeight;
-        zNear = 1;
-        zFar = zNear + D;
+        
+       
         var V = translate(0, 0, -0.5*(zNear + zFar)); 
         var P = perspective(fovy, aspect, zNear, zFar);
         var S = scalem(sun.radius); 
-        var t = 0.0;
-
-      
-    
+        
 
         sun.P = P;
-        sun.MV =  mult(V,S);
-        sun.render();
+        earth.P = P;
+        moon.P = P;
 
         const HoursPerDay = 24; 
-        const HoursPerYear = 365.25 /* days */ * HoursPerDay; 
+        const DaysPerYear = 365.25 /* days */ * HoursPerDay; 
       // Render the Earth next 
-        var day = t / HoursPerYear * 360; // in degrees 
-        var hour = t % HoursPerDay; 
-        var earthPos = translate(0.0, 0.0, earth.distance); 
-        var earthDay = rotate(day, [0, 1, 0]); 
-        var earthHour = rotate(hour, [0, 1, 0]); 
-        S = scalem(earth.radius); 
-        earth.P = P; 
-        // Earth's transforms: V * R * T * R * S 
-        earth.MV = mult( 
-                     mult( 
-                       mult( 
-                         // rotate to day of year 
-                         mult(V, earthDay), 
-                       earthPos), // move to orbit 
-                     earthHour), // rotate planet 
-                   S); // scale to size 
-        earth.render(); 
-
-        var moonPos = translate(0.0, 0.0, moon.distance); 
-        S = scalem(moon.radius); 
-        moon.P = P; 
-        // Moon's transforms: V * R * T * R * T * S 
-        moon.MV = mult( 
-                    mult( 
-                      mult( 
-                        mult( 
-                          // rotate to day of year 
-                          mult(V, earthDay), 
-                        earthPos), // move to orbit 
-                      earthHour), // rotate planet 
-                    moonPos), // move to Moon's orbit 
-                  S); // scale to size 
-        moon.render(); 
-
-    
-    
-    
-
-    
         
+        var year = t / DaysPerYear * 360; // in degrees 
+        var day = t % HoursPerDay; 
       
+          t = t + 1;
+      
+
     
         // Update your motion variables here
     
@@ -119,10 +82,10 @@ function init() {
         
         ms = new MatrixStack();
         var V = translate(0.0, 0.0, -0.5*(zNear + zFar));
-        
-        ms.scale(sun.radius, sun.radius, sun.radius);
-        
-
+        ms.load(V);
+        ms.push();
+        ms.scale(sun.radius);
+        sun.MV = ms.current();
         sun.render();
         ms.pop();
         ms.push();
@@ -131,10 +94,12 @@ function init() {
         ms.push();
         ms.rotate(day, axis);
         ms.scale(earth.radius);
+        earth.MV = ms.current();
         earth.render();
         ms.pop();
         ms.translate(moon.distance, 0, 0);
         ms.scale(moon.radius);
+        moon.MV = ms.current();
         moon.render();
         ms.pop();
     
