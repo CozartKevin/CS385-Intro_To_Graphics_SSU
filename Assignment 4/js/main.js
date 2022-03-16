279202b4cd9d2e89e1e0028b68f16d31cd28b484
Kevin Cozart <Cozartkevin@gmail.com>
2022-03-16 10:29:55 -0700

    Completely Lost
    No clue why it wont draw.  Not sure how to fix it. Will keep trying.

--- Assignment 4/js/main.js
+++ Assignment 4/js/main.js
@@ -15,9 +15,9 @@ function init() {
    // Earths Orbit : 1.5
    // moon orbit :  0.0175
    // Overall Diameter: 2.6102 * 2 = D
-   var sun = new Sphere;
-   var earth = new Sphere;
-   var moon = new Sphere;
+   var sun = new Sphere();
+   var earth = new Sphere();
+   var moon = new Sphere();
     //Sun Settings
     sun.radius = 1.09;
     sun.color = (0.970, 0.621, 0.165, 1);
@@ -30,76 +30,103 @@ function init() {
     moon.distance = 0.0175;
     moon.color = (0.860, 0.845, 0.826, 1)
 
-    sun.P = perspective(fovy, aspect, zNear, zFar);
-    earth.P = perspective(fovy, aspect, zNear, zFar);
-    moon.P = perspective(fovy, aspect, zNear, zFar);
-
-    sun.MV =  scalem(sun.raidus, sun.radius, sun.raius)
-    earth.MV = scalem(earth.radius, earth.radius, earth.radius);
-    moon.MV = scalem(moon.radius, moon.radius, moon.radius);
-
+   
     gl.viewport (0, 0, canvas.clientWidth, canvas.clientHeight);
-    var D = 2 * (earth.distance + moon.distance + moon.radius) 
-    var fovy, aspect, zNear, zFar; 
-    var tan = (D/2) / zNear + (D/2);
-    fovy = 2 * (1/tan);
-    aspect = canvas.clientWidth/canvas.clientHeight;
-    zNear = 1;
-    zFar = zNear + D;
+   
 
     // Add your sphere creation and configuration code here
     render = function(){
         var year = 0; 
         var day = 0;
-        var axis = 0; 
+        var axis = [1,0,0]; 
     
-        var D = 2 * (1.5 + 0.0175 + 0.0027) 
+        var D = 2 * (earth.distance + moon.distance + moon.radius); 
         var fovy, aspect, zNear, zFar; 
-        var tan = (D/2) / zNear + (D/2);
-        fovy = 2 * (1/tan);
+        var sin = (D/2) / (zNear + (D/2));
+        fovy = 2 * (Math.asin(sin));
         aspect = canvas.clientWidth/canvas.clientHeight;
         zNear = 1;
         zFar = zNear + D;
+        var V = translate(0, 0, -0.5*(zNear + zFar)); 
+        var P = perspective(fovy, aspect, zNear, zFar);
+        var S = scalem(sun.radius); 
+        var t = 0.0;
+
+        ms = new MatrixStack();
+    
+
+        sun.P = P;
+        sun.MV =  mult(V,S);
+        sun.render();
+
+        const HoursPerDay = 24; 
+        const HoursPerYear = 365.25 /* days */ * HoursPerDay; 
+      // Render the Earth next 
+        var day = t / HoursPerYear * 360; // in degrees 
+        var hour = t % HoursPerDay; 
+        var earthPos = translate(0.0, 0.0, earth.distance); 
+        var earthDay = rotate(day, [0, 1, 0]); 
+        var earthHour = rotate(hour, [0, 1, 0]); 
+        S = scalem(earth.radius); 
+        earth.P = P; 
+        // Earth's transforms: V * R * T * R * S 
+        earth.MV = mult( 
+                     mult( 
+                       mult( 
+                         // rotate to day of year 
+                         mult(V, earthDay), 
+                       earthPos), // move to orbit 
+                     earthHour), // rotate planet 
+                   S); // scale to size 
+        earth.render(); 
+
+        var moonPos = translate(0.0, 0.0, moon.distance); 
+        S = scalem(moon.radius); 
+        moon.P = P; 
+        // Moon's transforms: V * R * T * R * T * S 
+        moon.MV = mult( 
+                    mult( 
+                      mult( 
+                        mult( 
+                          // rotate to day of year 
+                          mult(V, earthDay), 
+                        earthPos), // move to orbit 
+                      earthHour), // rotate planet 
+                    moonPos), // move to Moon's orbit 
+                  S); // scale to size 
+        moon.render(); 
+
     
     
     
-        if(day === 366){
-           day = 0;
-            time = 0.0;
-            year++;
-        }
-    
+
     
         
-        var eye = [0,0,-0.5*(zNear + zFar)]
-        var look = [0,0,0];
-        var up = [0,0,1];
-       // var m = lookAt(eye, look, up);
-    
+      
     
         // Update your motion variables here
     
         gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
         
-        ms = new MatrixStack();
-    
+       
         var V = translate(0.0, 0.0, -0.5*(zNear + zFar));
-        ms.load(V);
-        ms.push();
-        ms.scale(sun.radius);
-        sun.draw;
+        
+        ms.scale(sun.radius, sun.radius, sun.radius);
+
+
+        sun.render();
         ms.pop();
         ms.push();
-        //ms.rotate(year, axis);
+        ms.rotate(year, axis);
         ms.translate(earth.distance, 0, 0);
         ms.push();
-       // ms.rotate(day, axis);
+        ms.rotate(day, axis);
         ms.scale(earth.radius);
-        earth.draw;
+        earth.render();
         ms.pop();
         ms.translate(moon.distance, 0, 0);
         ms.scale(moon.radius);
-        moon.draw;
+        moon.render();
         ms.pop();
     
     
@@ -108,69 +135,5 @@ function init() {
     requestAnimationFrame(render);
 }
 
-
-
-var year = 0; 
-var day = 0;
-var axis = 0; 
-function render() {
- 
-    var canvas = document.getElementById("webgl-canvas");
-    gl = canvas.getContext("webgl2");
-
-    var D = 2 * (1.5 + 0.0175 + 0.0027) 
-    var fovy, aspect, zNear, zFar; 
-    var tan = (D/2) / zNear + (D/2);
-    fovy = 2 * (1/tan);
-    aspect = canvas.clientWidth/canvas.clientHeight;
-    zNear = 1;
-    zFar = zNear + D;
-
-
-
-    if(day === 366){
-       day = 0;
-        time = 0.0;
-        year++;
-    }
-
-
-    
-    var eye = [0,0,-0.5*(zNear + zFar)]
-    var look = [0,0,0];
-    var up = [0,0,1];
-   // var m = lookAt(eye, look, up);
-
-
-    // Update your motion variables here
-
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    
-    ms = new MatrixStack();
-
-    var V = translate(0.0, 0.0, -0.5*(zNear + zFar));
-    ms.load(V);
-    ms.push();
-    ms.scale(sun.radius);
-    sun.draw();
-    ms.pop();
-    ms.push();
-    ms.rotate(year, axis);
-    ms.translate(earth.distance, 0, 0);
-    ms.push();
-    ms.rotation(day, axis);
-    ms.scale(earth.radius);
-    earth.draw();
-    ms.pop();
-    ms.translate(moon.distance, 0, 0);
-    ms.scale(moon.radius);
-    moon.draw();
-    ms.pop();
-
-
-    // Add your rendering sequence here
-
-    requestAnimationFrame(render);
-}
 
 window.onload = init;
